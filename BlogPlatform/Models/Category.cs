b7a4766dc8a0b5ec6922c517c9e09d2c8d94011c
Karlton Shoemaker <karltonshoemaker@gmail.com>
2021-03-05 14:45:40 -0500

    Passed category tests.

--- BlogPlatform/Models/Category.cs
+++ BlogPlatform/Models/Category.cs
@@ -7,6 +7,18 @@ namespace blog_template_practice.Models
 {
     public class Category
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
 
+        public Category()
+        {
+
+        }
+
+        public Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
     }
 }
